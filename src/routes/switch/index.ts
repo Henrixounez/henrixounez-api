36281212d2749ed1d8bfe392e15f4123b9169cce
Henrixounez <henrix.ounez@yahoo.fr>
2021-02-06 00:42:22 +0100

    add: id based client system

--- src/routes/switch/index.ts
+++ src/routes/switch/index.ts
@@ -2,24 +2,26 @@ import {Request, Response, NextFunction} from "express";
 import * as ws from 'ws';
 
 class SwitchMemory {
-  private _clients: ws[] = [];
+  private _clients: {id: number, client: ws}[] = [];
   private _value: boolean = false;
+  private _currId: number = 0;
 
   addToArray(c: ws) {
-    this._clients.push(c);
+    this._clients.push({id: this._currId, client: c});
+    return this._currId++;
   }
 
   changeValue(newValue: boolean) {
     this._value = newValue;
     this._clients.forEach((c) => {
       try {
-        c.send(JSON.stringify({value: this._value}));
+        c.client.send(JSON.stringify({value: this._value}));
       } catch (e) {}
     });
   }
 
-  removeClient(c: ws) {
-    this._clients = this._clients.filter((e) => e === c);
+  removeClient(id: number) {
+    this._clients = this._clients.filter((e) => e.id === id);
   }
 
   get value() {
@@ -30,19 +32,21 @@ class SwitchMemory {
 const Switch = new SwitchMemory();
 
 async function connect(ws: ws, req: Request, next: NextFunction) {
+  const id = Switch.addToArray(ws);
+
   ws.on('message', (msg) => {
+    console.log(id);
     Switch.changeValue(msg === "true");
   });
 
   ws.on('close', () => {
-    Switch.removeClient(ws);
+    Switch.removeClient(id);
   });
 
   ws.on('error', (err) => {
     console.log('[WS] Error', err);
   });
 
-  Switch.addToArray(ws);
   ws.send(JSON.stringify({value: Switch.value}));
 }
 
